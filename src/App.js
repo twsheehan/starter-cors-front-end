51dc2c38022dd536a6e295a0d0e98400b48860b6
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2020-12-04 14:44:40 -0600

    feat: add Home component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./home/Home";
+import header from "./header.jpg";
+
+const style = {
+  background: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)), url(${header})`,
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="jumbotron jumbotron-fluid text-white" style={style}>
+        <div className="container">
+          <h1 className="display-1">CORS TEST</h1>
+        </div>
+      </div>
+      <main className="container">
+        <Home />
+      </main>
+    </>
   );
 }
 
